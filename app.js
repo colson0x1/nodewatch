06bb20fbed570f2a2c7500a7dc8c4ec7a5699b31
Colson <colsoncalifornication@gmail.com>
2023-06-09 10:24:57 +0545

    create process with spawn() that returns a child process

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const prog = require('caporal');
 const fs = require('fs');
+const { spawn } = require('node:child_process');
+const importChalk = async () => {
+  try {
+    const chalk = await import('chalk');
+    return chalk.default;
+  } catch (error) {
+    console.error('Failed to import chalk:', error);
+    process.exit(1);
+  }
+};
 
 prog
   .version('0.0.1')
@@ -14,11 +24,19 @@ prog
     try {
       await fs.promises.access(name);
     } catch (err) {
-      throw new Error(`Could not find the file: ${name}`)
+      throw new Error(`Could not find the file: ${name}`);
     }
 
+    let proc;
+    const chalk = await importChalk();
     const start = debounce(() => {
-      console.log('STARTING YOUR NODE APP...');
+      if (proc) {
+        proc.kill();
+      }
+      const color = chalk.hex('#FFA500');
+      const message = '>>> Starting process...';
+      console.log(color(message));
+      proc = spawn('node', [name], { stdio: 'inherit' });
     }, 100);
 
     chokidar
---
+++ test.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log('hello world! im coding..');
+}, 1500);
